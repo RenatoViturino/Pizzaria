3f7fbbc065a9355e6385b7b653a336d6b8045b39
Renato Viturino <reviturino@gmail.com>
2026-06-22 20:58:05 -0300

    fix(test): corrige PizzariaApplicationTest e PizzariaApplicationTests com classes= da @SpringBootApplication

--- src/test/java/com/br/pizzaria/PizzariaApplicationTest.java
+++ src/test/java/com/br/pizzaria/PizzariaApplicationTest.java
@@ -2,11 +2,14 @@ package com.br.pizzaria;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = PizzariaApplication.class)
+@ActiveProfiles("test")
 class PizzariaApplicationTest {
 
     @Test
     void contextLoads() {
+        // verifica que o contexto Spring sobe sem erros
     }
 }
