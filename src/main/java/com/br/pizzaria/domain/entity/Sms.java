e3292d790dd6cf051ac8840c1dd0af744bbcbbed
Renato Viturino <reviturino@gmail.com>
2026-06-22 22:24:10 -0300

    merge

--- src/main/java/com/br/pizzaria/domain/entity/Sms.java
+++ src/main/java/com/br/pizzaria/domain/entity/Sms.java
@@ -1,17 +1,24 @@
 package com.br.pizzaria.domain.entity;
 
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
-public class SMS {
+/**
+ * Entidade de domínio: Sms
+ * PK gerada: idSms (Long)
+ */
+public class Sms {
 
+    @Setter
     private Long idSms;
     private String numero;
     private String mensagem;
     private LocalDateTime dataEnvio;
 
-    public SMS() {}
+    public Sms() {}
 
-    public SMS(String numero, String mensagem) {
+    public Sms(String numero, String mensagem) {
         this.numero = numero;
         this.mensagem = mensagem;
         this.dataEnvio = LocalDateTime.now();
@@ -19,10 +26,11 @@ public class SMS {
 
     public Long getIdSms() { return idSms; }
     public void setIdSms(Long idSms) { this.idSms = idSms; }
+
+
     public String getNumero() { return numero; }
     public void setNumero(String numero) { this.numero = numero; }
     public String getMensagem() { return mensagem; }
     public void setMensagem(String mensagem) { this.mensagem = mensagem; }
     public LocalDateTime getDataEnvio() { return dataEnvio; }
-    public void setDataEnvio(LocalDateTime dataEnvio) { this.dataEnvio = dataEnvio; }
 }
